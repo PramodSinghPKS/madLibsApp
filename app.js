c624027e1bb3734a6d4630dd8d7cf2c744f91d80
Pramod Kumar Singh <pramodkrsingh.pks@gmail.com>
2023-02-10 01:28:54 +0530

    add story is js file for reference

---
+++ app.js
@@ -0,0 +1,8 @@
+/*
+SAMPLE STORY
+
+'In ${userInputs[0]}, computer pioneer ${userInputs[1]} found herself working on a ${userInputs[2]}. 
+Word got out that the team had "debugged" the ${userInputs[2]}'
+
+*/
+
