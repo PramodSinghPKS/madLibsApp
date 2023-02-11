120df29394f9c863bfa607bbc70a05f3e1046e86
Pramod Kumar Singh <pramodkrsingh.pks@gmail.com>
2023-02-11 22:24:15 +0530

    add logic for user inputs

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
+// Varible to store user inputs
+let userInputs = [];
 
-let userInputs; //User inputs from page
-
+//Story
 let originalStory = `In ${userInputs[0]}, computer pioneer ${userInputs[1]} found herself working on a ${userInputs[2]}. 
 Word got out that the team had "debugged" the ${userInputs[2]}`;
 
+// number of unique replacements in story
 const numberOfQuestions = 3;
+
+// array of questions
+const questionArray = ["Enter a place","Enter a name","Enter a problem to solve"]
+
+// getting user input
+for (let i = 0; i < numberOfQuestions; i++) {
+    console.log(i);
+}
+
 
 /*
 STORY
