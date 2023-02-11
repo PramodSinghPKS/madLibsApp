b7341034622aae953709392ea8a98ce05540b556
Pramod Kumar Singh <pramodkrsingh.pks@gmail.com>
2023-02-11 22:14:08 +0530

    add mad-lib replacement variables to story

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-/*
-SAMPLE STORY
 
-'In ${userInputs[0]}, computer pioneer ${userInputs[1]} found herself working on a ${userInputs[2]}. 
-Word got out that the team had "debugged" the ${userInputs[2]}'
+let userInputs; //User inputs from page
+
+let originalStory = `In ${userInputs[0]}, computer pioneer ${userInputs[1]} found herself working on a ${userInputs[2]}. 
+Word got out that the team had "debugged" the ${userInputs[2]}`;
+
+/*
+STORY
+
+'In <someplace>, computer pioneer <somename> found herself working on a <a problem>. 
+Word got out that the team had "debugged" the <a problem>'
 
 */
-
